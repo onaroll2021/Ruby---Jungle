be3393047bfda02021380f173f62843d0282dd07
onaroll2021 <lukeli.onaroll@gmail.com>
2022-11-18 04:07:27 +0000

    add authentication for admin&products

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  
-  protect_from_forgery with: :exception
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_AUTH_USER"], password: ENV["HTTP_BASIC_AUTH_PASSWORD"]
 
+  protect_from_forgery with: :exception
 
   private
 
