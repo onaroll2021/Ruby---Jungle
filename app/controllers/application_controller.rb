a2460cec185f354c5690f6154790ee7f21dfddcc
onaroll2021 <lukeli.onaroll@gmail.com>
2022-11-22 02:48:37 +0000

    add user login/logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,15 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def authorize
+    redirect_to '/login' unless current_user
+  end
+
   private
 
   def cart
@@ -33,4 +42,7 @@ class ApplicationController < ActionController::Base
     }
     cookies[:cart]
   end
+
+
+
 end
