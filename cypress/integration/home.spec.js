d50ba31eabd694e172df4905d4b43d02b1b27a67
onaroll2021 <lukeli.onaroll@gmail.com>
2022-11-23 05:02:05 +0000

    complete Cypress-HomePage testing

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -9,4 +9,7 @@ describe('example to-do app', () => {
   it("There is products on the page", () => {
     cy.get(".products article").should("be.visible");
   });
+  it("There is 12 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
 })
