7b52d7b103b1683f9d08dd9e3897c3bc9dc479b9
onaroll2021 <lukeli.onaroll@gmail.com>
2022-11-23 06:52:46 +0000

    enable feature/testing-add-to-cart

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -1,4 +1,4 @@
-describe('example to-do app', () => {
+describe('Cypress test product detail', () => {
   beforeEach(() => {
     // Cypress starts out with a blank slate for each test
     // so we must tell it to visit our website with the `cy.visit()` command.
